d8f709520dc5f7fe11a691b1c081f951c8cefa35
pratapsantra <36pratap@gmail.com>
2025-11-01 09:43:16 +0530

    lottie file updated

--- src/screens/LoginScreen/index.tsx
+++ src/screens/LoginScreen/index.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   View,
   Text,
   TextInput,
   TouchableOpacity,
   StyleSheet,
+  ScrollView,
+  Pressable
 } from "react-native";
 // import Icon from "react-native-vector-icons/Ionicons";
 
@@ -17,7 +19,11 @@ import { RootState, AppDispatch } from '../../store/store';
 import { userLoginThunk } from '../../store/thunks/user.thunk';
 
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/FontAwesome'
+import IconMaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
+import LottieView from 'lottie-react-native';
 
 const LoginScreen = ({ navigation }: any) => {
   const [email, setEmail] = useState("p1@g.com");
@@ -25,6 +31,16 @@ const LoginScreen = ({ navigation }: any) => {
   const [showPassword, setShowPassword] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
   const [loginError, setLoginError] = useState<string | null>(null);
+
+  const animationRef = useRef<LottieView>(null);
+
+  useEffect(() => {
+    // animationRef?.current?.play(30);
+  }, []);
+
+  const pressOnHeart = () => {
+    animationRef?.current?.play();
+  }
 
   const handleLogin = async () => {
     // You can integrate your backend API here
@@ -55,62 +71,85 @@ const LoginScreen = ({ navigation }: any) => {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Welcome Back 👋</Text>
-      <Text style={styles.subtitle}>Login to your account</Text>
+    <ScrollView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.welcome}>
+          <LottieView style={{ flex: 1 }} source={require('../../assets/Welcome Animation.json')} autoPlay loop />
+          {/* <LottieView style={{ flex: 1 }} source={require('../../assets/loading_animation.json')} autoPlay loop /> */}
+        </View>
+        <Text style={styles.title}>Welcome Back 👋</Text>
+        <Text style={styles.subtitle}>Login to your account</Text>
 
-      <View style={styles.inputContainer}>
-        {/* <Icon name="mail-outline" size={20} color="#888" /> */}
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          placeholderTextColor="#888"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          value={email}
-          onChangeText={setEmail}
-        />
-      </View>
 
-      <View style={styles.inputContainer}>
-        {/* <Icon name="lock-closed-outline" size={20} color="#888" /> */}
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor="#888"
-          secureTextEntry={!showPassword}
-          value={password}
-          onChangeText={setPassword}
-        />
-        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-          {/* <Icon
+
+        <View style={styles.inputContainer}>
+          {/* <Icon name="mail-outline" size={20} color="#888" /> */}
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            placeholderTextColor="#888"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          {/* <Icon name="lock-closed-outline" size={20} color="#888" /> */}
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="#888"
+            secureTextEntry={!showPassword}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            {/* <Icon
             name={showPassword ? "eye-outline" : "eye-off-outline"}
             size={20}
             color="#888"
           /> */}
-        </TouchableOpacity>
-      </View>
-
-      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
-        <Text style={styles.loginText}>Login</Text>
-      </TouchableOpacity>
-      {loginError &&
-        <View>
-          <Text style={{ color: 'red', marginTop: 10, textAlign: 'center' }}>{loginError}</Text>
+          </TouchableOpacity>
         </View>
-      }
 
-      <TouchableOpacity onPress={() => navigation.navigate("Registration")}>
-        <Text style={styles.registerText}>
-          Don’t have an account? <Text style={styles.link}>Sign Up</Text>
-        </Text>
-      </TouchableOpacity>
-      <View>
-        <Text style={{ fontWeight: "bold", color: '#000' }}>BASE_URL:{Config.BASE_URL}</Text>
-        <Text style={{ fontWeight: "bold", color: '#000' }}>API_KEY:{Config.API_KEY}</Text>
-        <Text style={{ fontWeight: "bold", color: '#000' }}>ENVIRONMENT:{Config.ENVIRONMENT}</Text>
-      </View>
-    </SafeAreaView>
+        <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+          <Text style={styles.loginText}>Login</Text>
+        </TouchableOpacity>
+        {loginError &&
+          <View>
+            <Text style={{ color: 'red', marginTop: 10, textAlign: 'center' }}>{loginError}</Text>
+          </View>
+        }
+
+        <TouchableOpacity onPress={() => navigation.navigate("Registration")}>
+          <Text style={styles.registerText}>
+            Don’t have an account? <Text style={styles.link}>Sign Up</Text>
+          </Text>
+        </TouchableOpacity>
+
+        <View>
+          <Text style={{ fontWeight: "bold", color: '#000' }}>BASE_URL:{Config.BASE_URL}</Text>
+          <Text style={{ fontWeight: "bold", color: '#000' }}>API_KEY:{Config.API_KEY}</Text>
+          <Text style={{ fontWeight: "bold", color: '#000' }}>ENVIRONMENT:{Config.ENVIRONMENT}</Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Icon name="rocket" size={30} color="#900" />
+            <IconMaterialIcon name="emoji-people" size={30} color="#900" />
+            <IconMaterialIcon name="emoji-people" size={30} color="#900" />
+            <IconMaterialIcon name="add-shopping-cart" size={30} color="#900" />
+            <IconMaterialIcon name="sports-football" size={30} color="rgba(17, 182, 39, 1)" />
+            <FontAwesome5 name="youtube-square" size={30} color="rgba(221, 139, 16, 1)" />
+
+          </View>
+          <View style={styles.heart}>
+            <Pressable style={styles.heart1} onPress={pressOnHeart}>
+              <LottieView ref={animationRef} loop={false} style={{ flex: 1 }} source={require('../../assets/Heart fav.json')} />
+            </Pressable>
+          </View>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
@@ -169,5 +208,21 @@ const styles = StyleSheet.create({
   link: {
     color: "#4C8BF5",
     fontWeight: "600",
+  },
+  welcome: {
+    height: 300,
+    // aspectRatio:1,
+    justifyContent: 'center',
+    // backgroundColor:'#ff0000'
+  },
+  heart: {
+    flexDirection:'row',
+    justifyContent: 'space-around',
+    alignItems:'center'
+    
+  },
+  heart1: {
+    height: 200,
+     aspectRatio: 1,
   },
 });
